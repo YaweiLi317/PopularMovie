bda1309ee8c01d74fdaa87afb5334f2fad66b281
Leo Li <Leo Li>
2016-08-05 23:11:56 -0700

    Base Code

---
+++ app/src/main/java/app/com/example/android/popularmovie/MainActivity.java
@@ -0,0 +1,18 @@
+package app.com.example.android.popularmovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.main_activity, new MainFragment())
+                    .commit();
+        }
+    }
+}
